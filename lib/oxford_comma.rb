164d38b1702a0376b74362bd82f2694ae999331b
Neil Seejoor <nvseejoor@gmail.com>
2018-02-05 23:03:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    last = array.pop
+    ox_commas = array.map { |e| "#{e}, " }
+    ox_commas << "and #{last}"
+    return ox_commas.join
+  end
 end
